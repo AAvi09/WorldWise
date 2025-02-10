25c72b060b9f15e53bf5db7e354cdd52647f2cec
Avinash sharma <sharmaavi2196@gmail.com>
2025-02-10 21:06:40 +0530

    add layout and styling components; implement navigation and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
 import Products from "./pages/Products";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Products />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/AppNav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Styles from "./AppNav.module.css";
+
+const AppNav = () => {
+  return <nav className={Styles.nav}>App navigation</nav>;
+};
+
+export default AppNav;
