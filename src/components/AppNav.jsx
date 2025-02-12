f5375e04395d0e493ce69f97168d1cf032f5d5b9
Avinash sharma <sharmaavi2196@gmail.com>
2025-02-12 20:22:51 +0530

    add routing and navigation links; implement nested routes for cities and countries in App component, and update AppNav and SideBar components

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Styles from "./AppNav.module.css";
+import { NavLink } from "react-router-dom";
 
 const AppNav = () => {
-  return <nav className={Styles.nav}>App navigation</nav>;
+  return (
+    <nav className={Styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default AppNav;
