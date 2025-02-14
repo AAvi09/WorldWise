f3572f424928171bb12aa3a01bca6cc259762c05
Avinash sharma <sharmaavi2196@gmail.com>
2025-02-14 23:35:17 +0530

    enhance CityList component; pass cities and loading state as props, and implement CityItem for rendering city details

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CityItem = ({ cities }) => {
+  return <li>city</li>;
+};
+
+export default CityItem;
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import CityItem from "./CityItem";
 
-const CityList = () => {
-  return <ul className={styles.CityList}>LIST</ul>;
+const CityList = ({ cities, isLoading }) => {
+  if (isLoading) return <Spinner />;
+  return (
+    <ul className={styles.CityList}>
+      {cities?.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 };
 
 export default CityList;
