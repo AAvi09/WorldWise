9402df2beacd169dfa42421dbb27d566317e3937
Avinash sharma <sharmaavi2196@gmail.com>
2025-02-16 00:06:37 +0530

    enhance CityItem and Map components; include position in CityItem routing and implement latitude/longitude display in Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
 
 const Map = () => {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>map</h1>
+      <h1>
+        position : lat:{lat},lng:{lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        change pos
+      </button>
+    </div>
+  );
 };
 
 export default Map;
