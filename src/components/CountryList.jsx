2564643c523d5af746d4cc8c0c5bfc4689631c0b
Avinash sharma <sharmaavi2196@gmail.com>
2025-02-15 20:11:04 +0530

    add CountryList component; implement city detail routing and enhance CityItem for navigation

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Spinner from "./Spinner";
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+
+const CountryList = ({ cities, isLoading }) => {
+  if (isLoading) return <Spinner />;
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+  return (
+    <ul className={styles.countrylist}>
+      {countries.map((country) => (
+        <CountryItem country={country} />
+      ))}
+    </ul>
+  );
+};
+
+export default CountryList;
